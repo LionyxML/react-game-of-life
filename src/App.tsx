650220298cf24cbacf8d605c51a1cb45a4554f3f
LionyxML <rahul.juliato@gmail.com>
2023-01-01 20:15:18 -0300

    feat: gen counter, ease-out animation, new cell color

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,11 @@ import "./App.css";
 const DEFAULT_LINES = 20;
 const DEFAULT_COLS = 40;
 
-type lifeGridCell = {
-  line: number,
-  col: number,
-  key: string,
-  isAlive: boolean,
+type LifeGridCell = {
+  line: number;
+  col: number;
+  key: string;
+  isAlive: boolean;
 };
 
 const generateLifeCell = (line, col, isAlive) => ({
@@ -20,61 +20,38 @@ const generateLifeCell = (line, col, isAlive) => ({
   isAlive,
 });
 
-type lifeGridType = Array<
-  Array<{ line: number, col: number, key: string, isAlive: boolean }>
->;
+type LifeGrid = Array<Array<LifeGridCell>>;
 
-const generateLifeGrid = (
-  lines = DEFAULT_LINES,
-  columns = DEFAULT_COLS
-): lifeGridType =>
-  Array.from(Array(lines).fill(null), () => Array(columns).fill(null)).map(
-    (line, lineIndex) =>
-      line.map((_col, colIndex) => generateLifeCell(lineIndex, colIndex, false))
+const generateLifeGrid = (lines = DEFAULT_LINES, columns = DEFAULT_COLS): LifeGrid =>
+  Array.from(Array(lines).fill(null), () => Array(columns).fill(null)).map((line, lineIndex) =>
+    line.map((_col, colIndex) => generateLifeCell(lineIndex, colIndex, false)),
   );
 
 const countCellNeighbors = (
   line: number,
   col: number,
-  lifeGrid: lifeGridType,
-  isBorderLimited: boolean
+  lifeGrid: lifeGrid,
+  isBorderLimited: boolean,
 ): number =>
   [
-    isBorderLimited && line !== 0
-      ? pathOr(false, [line - 1, col - 1, "isAlive"], lifeGrid)
-      : false,
+    isBorderLimited && line !== 0 ? pathOr(false, [line - 1, col - 1, "isAlive"], lifeGrid) : false,
 
-    isBorderLimited && line !== 0
-      ? pathOr(false, [line - 1, col, "isAlive"], lifeGrid)
-      : false,
+    isBorderLimited && line !== 0 ? pathOr(false, [line - 1, col, "isAlive"], lifeGrid) : false,
 
-    isBorderLimited && line !== 0
-      ? pathOr(false, [line - 1, col + 1, "isAlive"], lifeGrid)
-      : false,
+    isBorderLimited && line !== 0 ? pathOr(false, [line - 1, col + 1, "isAlive"], lifeGrid) : false,
 
-    isBorderLimited && col !== 0
-      ? pathOr(false, [line, col - 1, "isAlive"], lifeGrid)
-      : false,
+    isBorderLimited && col !== 0 ? pathOr(false, [line, col - 1, "isAlive"], lifeGrid) : false,
 
     pathOr(false, [line, col + 1, "isAlive"], lifeGrid),
 
-    isBorderLimited && col !== 0
-      ? pathOr(false, [line + 1, col - 1, "isAlive"], lifeGrid)
-      : false,
+    isBorderLimited && col !== 0 ? pathOr(false, [line + 1, col - 1, "isAlive"], lifeGrid) : false,
 
-    isBorderLimited
-      ? pathOr(false, [line + 1, col, "isAlive"], lifeGrid)
-      : false,
+    isBorderLimited ? pathOr(false, [line + 1, col, "isAlive"], lifeGrid) : false,
 
-    isBorderLimited
-      ? pathOr(false, [line + 1, col + 1, "isAlive"], lifeGrid)
-      : false,
+    isBorderLimited ? pathOr(false, [line + 1, col + 1, "isAlive"], lifeGrid) : false,
   ].filter((x) => x).length;
 
-const generateFutureAliviness = (
-  isAlive: boolean,
-  neighbors: number
-): boolean => {
+const generateFutureAliviness = (isAlive: boolean, neighbors: number): boolean => {
   if (!isAlive && neighbors === 3) return true;
   if (isAlive && neighbors < 2) return false;
   if (isAlive && [2, 3].includes(neighbors)) return true;
@@ -90,10 +67,10 @@ const updateLifeGrid = (lifeGrid: lifeGridType, isBorderLimited: boolean) =>
         colIndex,
         generateFutureAliviness(
           cell.isAlive,
-          countCellNeighbors(lineIndex, colIndex, lifeGrid, isBorderLimited)
-        )
-      )
-    )
+          countCellNeighbors(lineIndex, colIndex, lifeGrid, isBorderLimited),
+        ),
+      ),
+    ),
   );
 
 function App() {
@@ -102,15 +79,10 @@ function App() {
   const [grid, setGrid] = useState(generateLifeGrid());
 
   const changeCurrentGrid = (line: number, col: number) => {
-    setGrid((currentGrid) => [
-      ...over(lensPath([line, col, "isAlive"]), not, currentGrid),
-    ]);
+    setGrid((currentGrid) => [...over(lensPath([line, col, "isAlive"]), not, currentGrid)]);
   };
 
-  const handleNextGeneration = (
-    grid: lifeGridType,
-    isBorderLimited: boolean
-  ) => {
+  const handleNextGeneration = (grid: lifeGridType, isBorderLimited: boolean) => {
     setGrid(updateLifeGrid(grid, isBorderLimited));
     setGeneration((generation) => generation + 1);
   };
@@ -123,12 +95,11 @@ function App() {
   return (
     <div className="App">
       <h1>
-        The <img src={reactLogo} className="logo react" alt="React logo" /> Game
-        of Life
+        The <img src={reactLogo} className="logo react" alt="React logo" /> Game of Life
       </h1>
       <p>
-        Click on a cell (or many cells) to populate it and use [Next Generation]
-        or other control button below.
+        Click on a cell (or many cells) to populate it and use [Next Generation] or other control
+        button below.
       </p>
       <div className="grid">
         {grid.map((line, i) => (
@@ -149,19 +120,7 @@ function App() {
       </div>
       <div className="card">
         <div className="line">
-          <div>
-            <label>
-              <input
-                type="checkbox"
-                checked={isBorderLimited}
-                onChange={() =>
-                  setBorderLimited((currentState) => !currentState)
-                }
-                disabled={true} // TODO: implement infinit border feature
-              />
-              Limit border
-            </label>
-          </div>
+          <p className="generation">Current Generation: {String(generation).padStart(3, "0")}</p>
 
           <button onClick={handleReset} disabled={generation === 0}>
             Reset
@@ -170,6 +129,18 @@ function App() {
           <button onClick={() => handleNextGeneration(grid, isBorderLimited)}>
             Next Generation
           </button>
+
+          <div>
+            <label>
+              <input
+                type="checkbox"
+                checked={isBorderLimited}
+                onChange={() => setBorderLimited((currentState) => !currentState)}
+                disabled={true} // TODO: implement infinit border feature
+              />
+              Limit border
+            </label>
+          </div>
         </div>
       </div>
 
